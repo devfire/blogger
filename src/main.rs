c11de8e7a1f632e5952a75396c205a6ec17a0de4
Igor Kantor <igor@devfire.io>
2022-11-25 20:18:55 -0500

    Random port (#5)
    * add tcpListener to run()
    
    * refactored tests
    
    * working tests
    
    * fixes
    
    * more test fixes
    
    * added empty response to subscribe()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::net::TcpListener;
+
 use blogger::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")
+    .expect("Failed to bind to a random port");
+
+    run(listener)?.await
 }
