e12f95013af3d140d91e4c85a404ed80f228d4f6
Igor <igor@devfire.io>
2022-11-26 12:42:16 -0500

    working subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
 
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
@@ -9,6 +18,7 @@ pub fn run() -> Result<Server, std::io::Error> {
     Ok(HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .bind("127.0.0.1:8000")?
     .run())
