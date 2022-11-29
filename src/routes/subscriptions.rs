023307fcee05f55eefaa6906dfdb2a354c8732e0
Igor <igor@devfire.io>
2022-11-29 18:18:46 -0500

    replaced match with if let Err

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,25 +10,23 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match sqlx::query!(
-        r#"
+    if let Err(e) = sqlx::query!(
+            r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
     "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now()
-    )
-    // We use `get_ref` to get an immutable reference to the `PgConnection`
-    // wrapped by `web::Data`.
-    .execute(pool.get_ref())
-    .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            println!("Failed to execute query: {}", e);
-            HttpResponse::InternalServerError().finish()
-        }
+            Uuid::new_v4(),
+            form.email,
+            form.name,
+            Utc::now()
+        )
+        // We use `get_ref` to get an immutable reference to the `PgConnection`
+        // wrapped by `web::Data`.
+        .execute(pool.get_ref())
+        .await {
+        println!("Failed to execute query: {}", e);
+        HttpResponse::InternalServerError().finish()
+    } else {
+        HttpResponse::Ok().finish()
     }
 }
