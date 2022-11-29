c3f430392bd984ab1305707e89e80047129dfa89
Igor Kantor <igor@devfire.io>
2022-11-28 21:31:15 -0500

    working config (#10)
    * initial config setup
    
    * with the config file
    
    * working config
    
    * added impl DatabaseSettings

---
+++ src/config.rs
@@ -0,0 +1,37 @@
+// reference: https://github.com/mehcode/config-rs/blob/master/examples/hierarchical-env/settings.rs
+use config::{Config, ConfigError};
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
+
+pub fn get_config() -> Result<Settings, ConfigError> {
+    
+    // add config values from a file named config.yaml
+    let settings = Config::builder()
+    .add_source(config::File::with_name("src/config.yaml"))
+    .build()?;
+    
+    settings.try_deserialize()
+}
