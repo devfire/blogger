6d0db4fe13c368315a1523b5302e8e2c691721a0
Igor <igor@devfire.io>
2022-11-29 18:34:45 -0500

    test fixed

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,43 +1,61 @@
-// use std::net::TcpListener;
+use blogger::config::get_config;
+use blogger::startup::run;
+use sqlx::PgPool;
+use std::net::TcpListener;
 
-// // if a test fails, we panic. No need to propagate errors
-// fn spawn_app() -> String {
-  
-//     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind");
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
 
-//     // retrieve the port chosen by the OS
-//     let port = listener.local_addr().unwrap().port();
+// if a test fails, we panic. No need to propagate errors
+async fn spawn_app() -> TestApp {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind");
 
-//     let server = blogger::startup::run(listener).expect("Failed to bind address");
+    // retrieve the port chosen by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let configuration = get_config().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    // return the struct combo with address + pool
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
+}
+
+#[tokio::test]
+async fn health_check_test() {
+    let app = spawn_app().await;
+
+    // issue requests against the app
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert_eq!(200, response.status().as_u16());
     
-//     // Launch the server as a background task
-//     // tokio::spawn returns a handle to the spawned future
-//     // except we don't need it, so _
-//     let _ = tokio::spawn(server);
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
 
-//     // run `cargo test -- --nocapture` to see the output
-//     println!("Binding to {}", port);
-
-//     // return the enum back to the caller
-//     format!("http://127.0.0.1:{}", port)
-// }
-
-// #[tokio::test]
-// async fn health_check_test() {
-//     let address = spawn_app();
-
-//     // issue requests against the app
-//     let client = reqwest::Client::new();
-
-//     let response = client
-//         .get(&format!("{}/health_check", &address))
-//         .send()
-//         .await
-//         .expect("Failed to execute request.");
-
-//         // make sure we get 200 back
-//         assert!(response.status().is_success());
-
-//         // make sure there's no body
-//         assert_eq!(Some(0), response.content_length());
-// }
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+}
