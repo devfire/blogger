c3942659978a3be142095940dc1e8f234d0cc598
Igor <igor@devfire.io>
2022-11-29 14:00:28 -0500

    added pgconnection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use std::net::TcpListener;
-use blogger::startup::run;
 use blogger::config::get_config;
+use blogger::startup::run;
+use sqlx::{Connection, PgConnection};
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    
     let config = get_config().expect("Failed to read config");
-    let address = format!("127.0.0.1:{}",config.application_port);
+    let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address)?;
+    let connection = PgConnection::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
-    run(listener)?.await
+    run(listener, connection)?.await
 }
